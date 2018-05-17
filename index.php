1ae6f5873fed14ad0b2339648a3a2e978e537de6
Dbirtic <thermalt4ke@gmail.com>
2018-05-17 17:23:14 +0200

    izmjenjen index.php

--- index.php
+++ index.php
@@ -58,10 +58,10 @@ register.</p>
  <?php
 // DB connection info
 // TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrievedearlier from the Azure Portal. $host = "value of Data Source";
-$host = "localhost";
-$user = "ruapuser";
+$host = "dbirtic-mysqldbserver.mysql.database.azure.com";
+$user = "ruapUser@dbirtic-mysqldbserver";
 $pwd = "AmLr.338";
-$db = "ruapuser";
+$db = "ruapdb";
 // Connect to database.
 $conn = mysqli_connect($host, $user, $pwd, $db);
 if (mysqli_connect_errno())
